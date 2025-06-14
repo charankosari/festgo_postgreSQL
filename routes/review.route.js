cd029da755072f78c46964c327f6284e700b00af
charankosari <shivacharankosari099@gmail.com>
2025-06-14 19:03:20 +0530

    added one more change

--- routes/review.route.js
+++ routes/review.route.js
@@ -4,33 +4,27 @@ const reviewController = require("../controllers/review.controller");
 const { isAuthorized } = require("../middlewares/auth");
 
 // Create Review
-router.post("/reviews", isAuthorized, reviewController.createReview);
+router.post("/", isAuthorized, reviewController.createReview);
 
 // Update Review
-router.put("/reviews/:id", isAuthorized, reviewController.updateReview);
+router.put("/:id", isAuthorized, reviewController.updateReview);
 
 // Delete Review
-router.delete("/reviews/:id", isAuthorized, reviewController.deleteReview);
+router.delete("/:id", isAuthorized, reviewController.deleteReview);
 
 // Get All Reviews
-router.get("/reviews", reviewController.getAllReviews);
+router.get("/", reviewController.getAllReviews);
 
 // Get Reviews by Property
-router.get(
-  "/reviews/property/:propertyId",
-  reviewController.getReviewsByProperty
-);
+router.get("/property/:propertyId", reviewController.getReviewsByProperty);
 
 // Get Average Rating of a Property
 router.get(
-  "/reviews/average/:propertyId",
+  "/average/:propertyId",
   reviewController.getAverageRatingByPropertyId
 );
 
 // Get Average Ratings of All Properties
-router.get(
-  "/reviews/averages",
-  reviewController.getAverageRatingOfAllProperties
-);
+router.get("/averages", reviewController.getAverageRatingOfAllProperties);
 
 module.exports = router;
