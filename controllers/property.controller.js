9cb9d12689f1b53d1755acea79c329f3988453d6
charankosari <shivacharankosari099@gmail.com>
2025-07-05 16:28:10 +0530

    some change

--- controllers/property.controller.js
+++ controllers/property.controller.js
@@ -1075,6 +1075,7 @@ exports.getPropertyRoomInventories = async (req, res) => {
           rates,
           defaultInventory,
           defaultRates,
+          beds: room.beds,
         });
       }
 
