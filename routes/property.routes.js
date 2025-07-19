86932d97cb4ab4102f9ea6ce5127140202a647f3
charankosari <shivacharankosari099@gmail.com>
2025-07-19 10:46:19 +0530

    some changes

--- routes/property.routes.js
+++ routes/property.routes.js
@@ -33,7 +33,7 @@ router.get(
 );
 router.get("/active", propertyController.getAllActiveProperties);
 router.post("/p/active-r", propertyController.getAllActivePropertiesByRange);
-router.post("/p/details", propertyController.getAmenitiesForProperty);
+// router.post("/p/details", propertyController.getAmenitiesForProperty);
 
 // ✅ Get property by ID
 router.get("/prop/:id", propertyController.getPropertyById);
