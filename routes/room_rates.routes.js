3b00b3b986bd4bf51f0e281ffbf231e8aa2d6cc6
charankosari <shivacharankosari099@gmail.com>
2025-08-01 12:34:51 +0530

    some change

--- routes/room_rates.routes.js
+++ routes/room_rates.routes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const rateController = require("../controllers/room_rate_inventory.controller");
-const { isAuthorized } = require("../middlewares/auth");
+const { isAuthorized, authorizedRoles } = require("../middlewares/auth");
 
 // 📌 Submit / Upsert room rates and inventory (bulk for date)
-router.post("/u", isAuthorized, rateController.submitRoomRates);
+router.post(
+  "/u",
+  isAuthorized,
+  authorizedRoles("admin", "vendor"),
+  rateController.submitRoomRates
+);
 
 module.exports = router;
